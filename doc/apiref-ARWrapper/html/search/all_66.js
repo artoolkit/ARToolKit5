1ecc11e07b19a3181299a3b4a4491776a2669fb0
Philip Lamb <phil.git@eden.net.nz>
2015-10-05 21:09:18 +1300

    Doc update.

--- doc/apiref-ARWrapper/html/search/all_66.js
+++ doc/apiref-ARWrapper/html/search/all_66.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['fastpath',['fastPath',['../class_video_source.html#ab3e908bae92d2b6573a1a82391c06600',1,'VideoSource']]],
   ['filtercutofffrequency',['filterCutoffFrequency',['../class_a_r_marker.html#aa3400dd1c820b3e6092ac66c922876cf',1,'ARMarker']]],
   ['filtersamplerate',['filterSampleRate',['../class_a_r_marker.html#a5eb97a8773789a2586f9bf469f657f96',1,'ARMarker']]],
   ['findmarker',['findMarker',['../class_a_r_controller.html#a586656329ba632f9f84cee4045fc6ff3',1,'ARController']]],
--- doc/apiref-ARWrapper/html/search/all_6d.js
+++ doc/apiref-ARWrapper/html/search/all_6d.js
@@ -4,6 +4,7 @@ var searchData=
   ['m_5fcf',['m_cf',['../class_a_r_marker_square.html#adca9e7eb79340e68aee462ebe93338ea',1,'ARMarkerSquare']]],
   ['m_5fcfmin',['m_cfMin',['../class_a_r_marker_square.html#a8dd5f5c936a3a3b8c9e9d3520c8ae54c',1,'ARMarkerSquare']]],
   ['m_5ferror',['m_error',['../class_video_source.html#a2b4216eef3515ec23ee509158fa4faf3',1,'VideoSource']]],
+  ['m_5ffastpath',['m_fastPath',['../class_video_source.html#add3b6650477bfd8686330f2a7bf9b421',1,'VideoSource']]],
   ['m_5fheight',['m_height',['../class_a_r_pattern.html#aa8569746e86c1f87931f4cc0cc76ccb1',1,'ARPattern']]],
   ['m_5fimage',['m_image',['../class_a_r_pattern.html#aec1d430d00d9b2d1db8b5a91cd2d7c1e',1,'ARPattern']]],
   ['m_5fimagesizex',['m_imageSizeX',['../class_a_r_pattern.html#aac22f3b642fb9e8d082205a00fee755b',1,'ARPattern']]],
