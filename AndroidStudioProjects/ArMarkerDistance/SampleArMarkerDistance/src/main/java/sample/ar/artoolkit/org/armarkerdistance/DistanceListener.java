d3c604f51f4b5304d3b7c011cdf73c31ce011cff
Thorsten Bux <teehorst@outlook.co.nz>
2016-01-27 16:40:41 +1300

    Added standard license and copyright header Removed unused interface

--- AndroidStudioProjects/ArMarkerDistance/SampleArMarkerDistance/src/main/java/sample/ar/artoolkit/org/armarkerdistance/DistanceListener.java
+++
@@ -1,9 +0,0 @@
-package sample.ar.artoolkit.org.armarkerdistance;
-
-/**
- * Created by Thorsten Bux on 19.01.2016.
- */
-public interface DistanceListener {
-
-    public void notify(float distance);
-}
