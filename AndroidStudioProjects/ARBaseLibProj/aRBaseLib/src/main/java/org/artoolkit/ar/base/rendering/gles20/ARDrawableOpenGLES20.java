6ba33d5bc7c72e1eaa50c2c593fea17be61f0adb
Thorsten Bux <thorsten.bux_c@daqri.com>
2016-02-11 12:49:01 +1300

    Added interface comment

--- AndroidStudioProjects/ARBaseLibProj/aRBaseLib/src/main/java/org/artoolkit/ar/base/rendering/gles20/ARDrawableOpenGLES20.java
+++ AndroidStudioProjects/ARBaseLibProj/aRBaseLib/src/main/java/org/artoolkit/ar/base/rendering/gles20/ARDrawableOpenGLES20.java
@@ -39,6 +39,14 @@ package org.artoolkit.ar.base.rendering.gles20;
 /**
  * Created by Thorsten Bux on 26.01.2016.
  */
+
+/**
+ * Interface should be used for all classed that paint some geometry in the space.
+ * Like {@link CubeGLES20} or {@link LineGLES20}
+ * It ensures that the needed methods {@link #draw(float[], float[])} and {@link #setShaderProgram(ShaderProgram)}
+ * are implemented by these classes.
+ *
+ */
 public interface ARDrawableOpenGLES20 {
     public void draw(float[] projectionMatrix, float[] modelViewMatrix);
     public void setShaderProgram(ShaderProgram program);
