1c637afc3058b728ce81b3e564f3b2eb65c1b0ac
Philip Lamb <phil.git@eden.net.nz>
2016-03-29 15:23:25 +1300

    Updates for Android NDK r11; enforce NDK r11 or later. fix minimum API version in EclipseProjects.

--- EclipseProjects/ARNative/jni/Application.mk
+++ EclipseProjects/ARNative/jni/Application.mk
@@ -54,5 +54,5 @@ APP_MODULES		:= ARNative
 #APP_ABI			:= all
 APP_ABI			:= armeabi armeabi-v7a x86 mips
 #APP_ABI			:= armeabi armeabi-v7a x86 mips arm64-v8a x86_64 mips64
-APP_PLATFORM    := android-8
+APP_PLATFORM    := android-15
 NDK_TOOLCHAIN_VERSION := clang
