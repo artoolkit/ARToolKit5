59a87270eee1af39af75cf2d94eb1783e73b3898
Thorsten Bux <thorsten.bux@outlook.com>
2017-12-12 13:19:26 +1300

    add windows powershell script for env variable

--- share/packaging/win-env.ps1
+++ share/packaging/win-env.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem Env:
-$env:HASH = git log --max-count=1 --pretty=format:"%H"
-$env:MESSAGE = git log --max-count=1 --pretty=format:"%s"
+# $env:HASH = git log --max-count=1 --pretty=format:"%H"
+# $env:MESSAGE = git log --max-count=1 --pretty=format:"%s"
 $env:BUILDTIME = [int][double]::Parse((Get-Date -UFormat %s))
+Write-Host "##vso[task.setvariable variable=BUILDTIME]$Env:BUILDTIME"
